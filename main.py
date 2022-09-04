576a1640d66909ee8228b5c1af711c1dd6526b8c
Gabitob <gabitob@live.com>
2022-09-04 13:44:08 -0300

    Função Deletar linha

--- main.py
+++ main.py
@@ -23,12 +23,43 @@ except sr.RequestError as e:
 
 
 file_path = 'teste.txt'
-s.say('Você disse' + texto)
-s.runAndWait()
 
 if texto == 'limpar':
+    s.say('Você disse' + texto)
+    s.runAndWait()
     with open(file_path, "w") as f:
         pass
+
+elif texto == 'deletar':
+    s.say('Você disse' + texto)
+    s.runAndWait()
+    s.say('Escolha o número da linha')
+    s.runAndWait()
+
+    with sr.Microphone() as source:
+        print()
+        print("Escolha o número da linha")
+        print()
+        audio = r.listen(source)
+
+    texto2 = r.recognize_google(audio, language='pt-BR')
+    numLinha = int(texto2)
+    try:
+        with open('teste.txt', 'r') as fr:
+            linhas = fr.readlines()
+
+            ptr = 1
+
+            with open('teste.txt', 'w') as fw:
+                for linha in linhas:
+                    if ptr != numLinha:
+                        fw.write(linha)
+                    ptr += 1
+        print("Linha" + texto2 + "deletada")
+        s.say('Linha' + texto2 + 'deletada')
+        s.runAndWait()
+    except:
+        print("Algo deu errado!")
 else:
     sys.stdout = open(file_path, "a")
     print(texto)
